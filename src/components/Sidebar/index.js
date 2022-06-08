a8757795972ed97153e5fe576b26e423ece11ddb
kamath7 <adithyakamath96@gmail.com>
2022-06-08 21:10:11 +0530

    amendements and layout added

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,22 +1,70 @@
-import React from "react";
-import { Link, NavLink } from "react-router-dom";
-import LogoSmall from "../../assets/images/lalle1.jpg";
-import LogoSubs from "../../assets/images/logo_sub.png";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHome } from "@fortawesome/free-brands-svg-icons";
-import "../Sidebar/index.scss";
+import './index.scss'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faLinkedin,
+  faGithub,
+  faInstagram
+} from '@fortawesome/free-brands-svg-icons'
+import { faHome, faUser, faEnvelope, faSuitcase } from '@fortawesome/free-solid-svg-icons'
+import { Link, NavLink } from 'react-router-dom'
 const Sidebar = () => {
   return (
     <div className="nav-bar">
       <Link className="logo" to="/">
-        <img src={LogoSmall} alt="site logo" />
-        <img className="sub-logo" src={LogoSubs} alt="rk" />
+        <img src={LogoS} alt="site logo" />
+        <img className="sub-logo" src={LogoSubtitle} alt="rk" />
       </Link>
       <nav>
         <NavLink exact="true" activeclassname="active" to="/">
-
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink activeclassname="active" className="about-link" to="/about">
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink activeclassname="active" className="portfolio-link" to="/portfolio">
+          <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
         </NavLink>
       </nav>
+      <ul>
+        <li>
+        <a
+            href="https://in.linkedin.com/in/rithu-anand-krishnan-907786ab"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+
+        <li>
+          <a
+            href="https://github.com/rithukrishnan"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+
+        <li>
+          <a
+            href="https://www.instagram.com/rithukrishnan/?hl=en"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faInstagram} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
     </div>
   );
 };
